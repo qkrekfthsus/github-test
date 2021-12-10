fa4db90062f7feef9ee4ed2cbd9e85a6368b101d
qkrekfthsus <qkrekfthsus@naver.com>
2021-12-10 10:28:10 +0900

    작성자 : 이종표 날짜 : 2021-12-10 작업 내용 : TestDto 추가

---
+++ src/main/java/com/pyo/test/dto/TestDto.java
@@ -0,0 +1,53 @@
+package com.pyo.test.dto;
+
+public class TestDto {
+	
+	private String testName;
+	private String testTime;
+	private String studentName;
+	private int score;
+	
+	public TestDto() {}
+
+	public TestDto(String testName, String testTime, String studentName, int score) {
+		super();
+		this.testName = testName;
+		this.testTime = testTime;
+		this.studentName = studentName;
+		this.score = score;
+	}
+
+	public String getTestName() {
+		return testName;
+	}
+
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+
+	public String getTestTime() {
+		return testTime;
+	}
+
+	public void setTestTime(String testTime) {
+		this.testTime = testTime;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+}
